19a828ef4c0b2563c27fa5facf449726f76cf9e3
Muhammad Rafly Mumtaz <raflymumtaz23@gmail.com>
2024-06-21 12:25:24 +0700

    loginlegister

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, ButtonModule],
 })
 export class HomeComponent {
 
